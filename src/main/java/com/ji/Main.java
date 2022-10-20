c8c431dd259c09ef70920852efd8d23b33b46471
Luca Coraci <luca.coraci@gmail.com>
2022-10-21 00:15:42 +0200

    provolo

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -2,6 +2,7 @@ package com.ji;
 
 public class Main {
 
+    //prova status
     public static final String API_KEY = "8371bcc0-50ba-11ed-8140-7824af89bb2d";
     private String password = "testGitGuard4an";
 
