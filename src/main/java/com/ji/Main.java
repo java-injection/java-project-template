cd7e7838ae93a38cf70fd99c84f5cff646f2369d
Luca Coraci <luca.coraci@gmail.com>
2022-10-20 23:04:38 +0200

    test guardian

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -1,6 +1,10 @@
 package com.ji;
 
 public class Main {
+
+    public static final String API_KEY = "REDACTED";
+    private String password = "testGitGuard1an";
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
