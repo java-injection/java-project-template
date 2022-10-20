53243769884e284aac41c043b1b2dacb0ad9cd66
Luca Coraci <luca.coraci@gmail.com>
2022-10-20 22:12:01 +0200

    first commit

---
+++ src/main/java/com/ji/Main.java
@@ -0,0 +1,7 @@
+package com.ji;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/ji/sample/PersonTest.java
@@ -0,0 +1,40 @@
+package com.ji.sample;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+        Person p = new Person("Luca","Coraci");
+        assertEquals("Luca",p.getName());
+    }
+
+    @Test
+    void setName() {
+        Person p = new Person("Luca","Coraci");
+        assertEquals("Luca",p.getName());
+        p.setName("Alfonso");
+        assertEquals("Alfonso",p.getName());
+    }
+
+    @Test
+    void getSurname() {
+    }
+
+    @Test
+    void setSurname() {
+    }
+}
