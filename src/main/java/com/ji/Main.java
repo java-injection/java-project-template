f6d5b73db2beb50dd2fecc3da1f573c6e52cca36
Luca Coraci <luca.coraci@gmail.com>
2022-10-21 01:36:22 +0200

    GG2

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -2,7 +2,7 @@ package com.ji;
 
 public class Main {
 
-    //webhooked 3
+    //webhooked 4
     public static final String API_KEY = "REDACTED";
     private String password = "testGitGuard4an";
 
