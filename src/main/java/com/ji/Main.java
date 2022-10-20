49dfdf64ef932dd9e1496ac332506e45fe49d8ed
Luca Coraci <luca.coraci@gmail.com>
2022-10-21 01:25:10 +0200

    last test

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -2,7 +2,7 @@ package com.ji;
 
 public class Main {
 
-    //webhooked 2
+    //webhooked 3
     public static final String API_KEY = "REDACTED";
     private String password = "testGitGuard4an";
 
