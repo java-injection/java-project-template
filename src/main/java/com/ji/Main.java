ac69dcc59165a738d888ce3a64fcf6dd4a954621
Luca <luca.coraci@gmail.com>
2022-10-21 18:58:00 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     //hello hacker +39
     public static final String API_KEY = "REDACTED";
-    private final String password = "testGitvGuard4an";
+    private final String password = "testGitGuard4an";
 
     public static void main(String[] args) {
         System.out.println("Hello world!!");
