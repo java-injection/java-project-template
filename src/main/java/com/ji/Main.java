14060e9a1e9703d28175e7a39e27ef02b098c6d0
sommovir <luca.coraci@gmail.com>
2022-10-21 18:49:03 +0200

    Update Main.java

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -7,6 +7,6 @@ public class Main {
     private final String password = "testGitvGuard4an";
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
