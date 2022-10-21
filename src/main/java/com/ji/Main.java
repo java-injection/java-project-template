6cecfec40c14dd2e894f30d8008056418d0f1298
Luca <luca.coraci@gmail.com>
2022-10-21 15:30:55 +0200

    testing sms phase 1

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     //testing sms
     public static final String API_KEY = "8371bcc0-50ba-11ed-8140-7824af89bb2d";
-    private String password = "testGitGuard4an";
+    private final String password = "testGitGuard4an";
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
