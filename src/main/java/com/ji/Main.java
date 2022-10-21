894f4b22b0054708c62034af2e607048f7a58cbf
Luca <luca.coraci@gmail.com>
2022-10-21 18:58:57 +0200

    no key

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -3,8 +3,6 @@ package com.ji;
 public class Main {
 
     //hello hacker +39
-    public static final String API_KEY = "REDACTED";
-    private final String password = "testGitGuard4an";
 
     public static void main(String[] args) {
         System.out.println("Hello world!!");
