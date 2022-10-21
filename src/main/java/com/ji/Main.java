f999353a6c0900162bbb6e3e2d1a2976957a0b9d
sommovir <luca.coraci@gmail.com>
2022-10-21 18:47:40 +0200

    Update Main.java

--- src/main/java/com/ji/Main.java
+++ src/main/java/com/ji/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     //hello hacker +39
     public static final String API_KEY = "8371bcc0-50ba-11ed-8140-7824af89bb2d";
-    private final String password = "testGitGuard4an";
+    private final String password = "testGitvGuard4an";
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
